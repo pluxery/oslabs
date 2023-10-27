033dc7ece770ffdb602c2cf518f0fa412483441c
Dranov Semen <84563687+pluxery@users.noreply.github.com>
2023-10-27 12:24:48 +1000

    Update main.cpp
    .

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -3,213 +3,246 @@
 #include <sstream>
 #include <chrono>
 #include <thread>
+#include <ctime>
+#include <cstdlib>
+#include <cstring>
 
-#ifdef _WIN32
-#include <Windows.h>
+#if defined(_WIN32)
+#include <windows.h>
+#include <process.h>
+#define getpid() GetCurrentProcessId()
 #else
 
 #include <unistd.h>
-#include <iomanip>
+#include <sys/types.h>
+#include <sys/time.h>
 
 #endif
 
-class Logger {
-private:
-    std::ofstream logFile;
-
+class Logger
+{
 public:
-    Logger(const std::string &filePath) {
-        logFile.open(filePath, std::ios_base::app);
+    Logger(const std::string &fileName)
+    {
+        logFile.open(fileName, std::ios_base::app);
+        if (!logFile.is_open())
+        {
+            std::cout << "Failed to open log file." << std::endl;
+            exit(1);
+        }
     }
 
-    ~Logger() {
-        if (logFile.is_open()) {
+    void writeToLog(const std::string &message)
+    {
+        logFile << message << std::endl;
+    }
+
+    ~Logger()
+    {
+        if (logFile.is_open())
+        {
             logFile.close();
         }
     }
 
-    void writeLog(const std::string &message) {
-        if (logFile.is_open()) {
-            logFile << message << std::endl;
-        }
+private:
+    std::ofstream logFile;
+};
+
+class CopyProcess
+{
+public:
+    CopyProcess(int &counter, Logger &logger)
+        : counter(counter), logger(logger) {}
+
+    void run()
+    {
+        onStart();
+
+        performCopy();
+
+        onExit();
+    }
+
+protected:
+    virtual void onStart()
+    {
+        std::ostringstream os;
+        os << "Process: Process ID=" << getpid() << ", Start Time=" << time(NULL) << std::endl;
+        logger.writeToLog(os.str());
+    }
+
+    virtual void performCopy() = 0;
+
+    virtual void onExit()
+    {
+        std::ostringstream os;
+        os << "Process: Exit Time=" << time(NULL) << std::endl;
+        logger.writeToLog(os.str());
+    }
+
+protected:
+    int &counter;
+    Logger &logger;
+};
+
+class Copy1Process : public CopyProcess
+{
+public:
+    Copy1Process(int &counter, Logger &logger)
+        : CopyProcess(counter, logger) {}
+
+protected:
+    void performCopy() override
+    {
+        std::ostringstream os;
+        os << "Copy 1: Counter increased by 10" << std::endl;
+        counter += 10;
+        logger.writeToLog(os.str());
     }
 };
 
-class TimeUtility {
+class Copy2Process : public CopyProcess
+{
 public:
-    static std::string getCurrentTime() {
-        std::stringstream ss;
-        auto now = std::chrono::system_clock::now();
-        auto timeNow = std::chrono::system_clock::to_time_t(now);
-        auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()) % 1000;
+    Copy2Process(int &counter, Logger &logger)
+        : CopyProcess(counter, logger) {}
 
-#ifdef _WIN32
-        struct tm timeInfo;
-        localtime_s(&timeInfo, &timeNow);
+protected:
+    void performCopy() override
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(3000));
+
+        std::ostringstream os;
+        os << "Copy 2: Counter doubled" << std::endl;
+        counter *= 2;
+        logger.writeToLog(os.str());
+
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+
+        os.str("");
+        counter /= 2;
+        os << "Copy 2: Counter halved" << std::endl;
+        logger.writeToLog(os.str());
+    }
+};
+
+class CommandProcess
+{
+public:
+    CommandProcess(int &counter, Logger &logger)
+        : counter(counter), logger(logger) {}
+
+    void run()
+    {
+        std::string input;
+        std::cout << "Enter command: exit, <counter value>" << std::endl;
+        std::getline(std::cin, input);
+
+        if (!input.empty())
+        {
+            if (input == "exit")
+            {
+                exit(0);
+            }
+            counter = std::stoi(input);
+        }
+    }
+
+private:
+    int &counter;
+    Logger &logger;
+};
+
+class MainProcess
+{
+public:
+    MainProcess(Logger &logger)
+        : counter(0), logger(logger) {}
+
+    void run()
+    {
+        std::thread copy1Thread, copy2Thread, commandThread;
+
+        std::ostringstream os;
+        os << "Main process: Process ID=" << getpid() << ", Start Time=" << time(NULL) << std::endl;
+        logger.writeToLog(os.str());
+
+        while (true)
+        {
+            counter++;
+            std::this_thread::sleep_for(std::chrono::milliseconds(300));
+
+            time_t currentTime = time(NULL);
+            struct tm *timeinfo;
+            char dateTimeBuffer[80];
+
+            timeinfo = localtime(&currentTime);
+            strftime(dateTimeBuffer, sizeof(dateTimeBuffer), "%Y-%m-%d %H:%M:%S", timeinfo);
+
+            os.str("");
+            os << dateTimeBuffer << ".";
+
+#if defined(_WIN32)
+            SYSTEMTIME sysTime;
+            GetSystemTime(&sysTime);
+            os << sysTime.wMilliseconds;
 #else
-        struct tm timeInfo;
-        localtime_r(&timeNow, &timeInfo);
+            struct timeval timeVal;
+            gettimeofday(&timeVal, NULL);
+            os << timeVal.tv_usec / 1000;
 #endif
 
-        ss << std::put_time(&timeInfo, "%Y-%m-%d %H:%M:%S");
-        ss << '.';
-        ss << std::setfill('0') << std::setw(3) << ms.count();
+            os << ": Process ID=" << getpid() << ", Counter=" << counter << std::endl;
+            logger.writeToLog(os.str());
 
-        return ss.str();
-    }
-};
-
-class Timer {
-private:
-    int &counter;
-    bool &allowCounterModification;
-    std::chrono::milliseconds interval;
-    std::thread timerThread;
-
-public:
-    Timer(int &counter, bool &allowCounterModification)
-            : counter(counter), allowCounterModification(allowCounterModification), interval(300) {}
-
-    ~Timer() {
-        if (timerThread.joinable()) {
-            timerThread.join();
-        }
-    }
-
-    void start() {
-        timerThread = std::thread([this]() {
-            while (allowCounterModification) {
-                std::this_thread::sleep_for(interval);
-                if (allowCounterModification) {
-                    ++counter;
-                }
+            if (!copy1Thread.joinable())
+            {
+                copy1Thread = std::thread([&]()
+                                          {
+                    Copy1Process copy1(counter, logger);
+                    copy1.run();
+                    copy1Thread.detach(); });
             }
-        });
-    }
-};
+            else
+            {
+                logger.writeToLog("copy1 running");
+            }
 
-class Copy {
-private:
-    std::string processId;
-    int &counter;
-    bool &allowCounterModification;
-    Logger &logger;
+            if (!copy2Thread.joinable())
+            {
+                copy2Thread = std::thread([&]()
+                                          {
+                    Copy2Process copy2(counter, logger);
+                    copy2.run();
+                    copy2Thread.detach(); });
+            }
+            else
+            {
+                logger.writeToLog("copy2 running");
+            }
 
-public:
-    Copy(const std::string &processId, int &counter, bool &allowCounterModification, Logger &logger)
-            : processId(processId), counter(counter), allowCounterModification(allowCounterModification),
-              logger(logger) {}
-
-    void run() {
-        std::string copyId;
-        std::stringstream copyMessage;
-
-        // Generate unique copyId
-        {
-            std::stringstream ss;
-            ss << processId << "_" << std::this_thread::get_id();
-            copyId = ss.str();
+            if (!commandThread.joinable())
+            {
+                commandThread = std::thread([&]()
+                                            {
+                    CommandProcess command(counter, logger);
+                    command.run();
+                    commandThread.detach(); });
+            }
         }
-
-        // Calculate modified counter value
-        int copyCounter = counter;
-        if (copyId.length() % 2 == 0) {
-            copyCounter += 10;
-        } else {
-            copyCounter *= 2;
-        }
-
-        // Prepare copy message
-        copyMessage << "Copy " << copyId << " started at " << TimeUtility::getCurrentTime() << std::endl;
-        std::this_thread::sleep_for(std::chrono::seconds(2));  // Simulate some work
-        copyMessage << "Modified counter value: " << copyCounter << std::endl;
-        copyMessage << "Copy " << copyId << " exited at " << TimeUtility::getCurrentTime();
-
-        // Write copy message to log
-        logger.writeLog(copyMessage.str());
     }
-};
 
-class Program {
 private:
     int counter;
-    bool allowCounterModification;
-    bool canSpawnCopies;
-    Logger logger;
-    Timer timer;
-
-public:
-    Program(const std::string &logFilePath)
-            : counter(0), allowCounterModification(false), canSpawnCopies(true), logger(logFilePath),
-              timer(counter, allowCounterModification) {}
-
-    void run() {
-        // Get process ID
-#ifdef _WIN32
-        DWORD processId = GetCurrentProcessId();
-#else
-        pid_t processId = getpid();
-#endif
-
-        // Open log file and write startup message
-        logger.writeLog("Process ID: " + std::to_string(processId) + ", started at " + TimeUtility::getCurrentTime());
-
-        timer.start();
-
-        while (true) {
-            // Check if any copy is still running
-            if (!allowCounterModification) {
-                std::cout << "Cannot spawn copies. A copy is still running." << std::endl;
-            }
-
-            // Process user command
-            std::string command;
-            std::cout << "Enter command (set_counter, spawn_copies, quit): ";
-            std::cin >> command;
-
-            if (command == "set_counter") {
-                std::cout << "Enter new counter value: ";
-                std::cin >> counter;
-                allowCounterModification = true;
-            } else if (command == "spawn_copies") {
-                if (allowCounterModification) {
-                    allowCounterModification = false;
-
-                    // Spawn copies
-                    Copy copy1(std::to_string(processId), counter, allowCounterModification, logger);
-                    Copy copy2(std::to_string(processId), counter, allowCounterModification, logger);
-
-                    std::thread copy1Thread(&Copy::run, &copy1);
-                    std::thread copy2Thread(&Copy::run, &copy2);
-
-                    copy1Thread.join();
-                    copy2Thread.join();
-                } else {
-                    std::cout << "Cannot spawn copies. Counter modification is not allowed." << std::endl;
-                }
-            } else if (command == "quit") {
-                // Exit the program
-                allowCounterModification = false;
-                canSpawnCopies = false;
-                break;
-            } else {
-                std::cout << "Invalid command." << std::endl;
-            }
-
-            // Write current time, process ID, and counter value to log
-            logger.writeLog(TimeUtility::getCurrentTime() + ", Process ID: " + std::to_string(processId)
-                            + ", Counter: " + std::to_string(counter));
-            std::this_thread::sleep_for(std::chrono::seconds(1));
-        }
-
-        // Write exit message to log file
-        logger.writeLog("Process ID: " + std::to_string(processId) + " exited at " + TimeUtility::getCurrentTime());
-    }
+    Logger &logger;
 };
 
-int main() {
-    Program program("log.txt");
-    program.run();
+int main()
+{
+    Logger logger("log.txt");
+    MainProcess mainProcess(logger);
+    mainProcess.run();
 
     return 0;
 }
